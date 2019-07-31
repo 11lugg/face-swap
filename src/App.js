e4a67e5933df81636132eb44a851e372466e2d8d
Jon Lugg <jon.lugg@Nology.local>
2019-07-31 12:43:03 +0100

    Auth added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import ScaryFace from "./components/ScaryFace";
+import googleIcon from "./assets/google-icon.png";
+import firebase, { provider } from "./firebase";
 
-function App() {
-  return (
-    <div>
-      <ScaryFace rotate={1800} />
-      <ScaryFace rotate={12} />
-      <ScaryFace rotate={70} />
-      <ScaryFace rotate={3000000} />
-      <ScaryFace rotate={250} />
-      <ScaryFace rotate={70} />
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: null,
+    error: {}
+  };
+
+  signIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        this.setState({
+          user
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const notLoggedIn = "Sign in with google";
+    const loggedIn = "Sign out";
+    const changeBtnText = this.state.user ? loggedIn : notLoggedIn;
+    return (
+      <div>
+        <div>
+          <ScaryFace rotate={1800} />
+          <ScaryFace rotate={12} />
+          <ScaryFace rotate={70} />
+          <ScaryFace rotate={3000000} />
+          <ScaryFace rotate={250} />
+          <ScaryFace rotate={70} />
+        </div>
+        <div onClick={this.signIn}>
+          <img src={googleIcon} alt="" />
+          <button>{changeBtnText}</button>
+        </div>
+      </div>
+    );
+  }
+
+  handleSubmit = event => {
+    this.props.authenticateUser(this.state.email, this.state.pass);
+    event.preventDefault();
+  };
 }
-
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
+
+// Basic firebase configuration linking web application to database - see firebase documentation
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrETpcAtzH0SF-QkbbM0ErWj9pbPLnusk",
+  authDomain: "faceapp-9e877.firebaseapp.com",
+  databaseURL: "https://faceapp-9e877.firebaseio.com",
+  projectId: "faceapp-9e877",
+  storageBucket: "",
+  messagingSenderId: "526810962930",
+  appId: "1:526810962930:web:16d25700e86f216d"
+};
+
+// Initialising firebase
+firebase.initializeApp(firebaseConfig);
+
+// Exporting connection to provider for google signin
+export const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({
+  login_hint: "user@example.com"
+});
+// Exporting connection to database as a variable
+export const firestore = firebase.firestore();
+
+export default firebase;
